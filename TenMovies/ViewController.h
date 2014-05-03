6387e1cbd4ac709551e90de271ea2e327161114c
Scott Kensell <scott.kensell@prezi.com>
2014-05-03 20:07:26 +0200

    Initial Commit

---
+++ TenMovies/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TenMovies
+//
+//  Created by Scott Kensell on 5/3/14.
+//  Copyright (c) 2014 Scott Kensell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TenMovies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TenMovies
+//
+//  Created by Scott Kensell on 5/3/14.
+//  Copyright (c) 2014 Scott Kensell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
